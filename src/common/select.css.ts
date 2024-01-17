bb337844571bccc68bf3264dc70523692b1acec6
celina-ijs <celina.dev@instajs.com>
2024-01-17 12:37:04 +0700

    Update scroll on select
    Update scroll
    
    Update code

--- src/common/select.css.ts
+++ src/common/select.css.ts
@@ -3,7 +3,7 @@ import { Styles } from '@ijstech/components';
 export const transitionStyle = Styles.style({
   $nest: {
     '.scroll-container': {
-      transition: 'transform 0.3s cubic-bezier(0.4, 0, 0.2, 1)',
+      // transition: 'transform .3s cubic-bezier(0.19, 1, 0.22, 1) 0s',
       $nest: {
         '&::-webkit-scrollbar': {
           height: 0,
